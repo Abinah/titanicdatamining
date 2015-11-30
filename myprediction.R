8387db5d5fe6a93d79aa6bce1b55b5f77a6310f5
Abinah <abnergonyi@gmail.com>
2015-11-30 05:42:10 +0300

    predicted and saved my output as a csv file

---
+++ myprediction.R
@@ -0,0 +1,10 @@
+my_prediction <- predict(my_tree_two, test, type = "class")
+
+# Create a data frame with two columns: PassengerId & Survived. Survived contains your predictions
+my_solution <- data.frame(PassengerId = test$PassengerId, Survived = my_prediction)
+
+# Check that your data frame has 418 entries
+nrow(my_solution)
+
+# Write your solution to a csv file with the name my_solution.csv
+write.csv(my_solution, file = "my_solution.csv", row.names = FALSE)
